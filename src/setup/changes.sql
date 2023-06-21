5d4729b412440acdba6a8c070852142e9146e9d9
Bunyodbek <93827769+BunyodbekNasriddinov@users.noreply.github.com>
2023-06-21 17:02:18 +0500

    add: setup.sql

--- src/setup/changes.sql
+++ src/setup/changes.sql
@@ -1,2 +1,9 @@
 ALTER TABLE admins 
 ALTER COLUNM admin_role TYPE VARCHAR(15)
+
+-- super admin table
+CREATE TABLE superadmin(
+  id SERIAL NOT NULL,
+  username VARCHAR(32) NOT NULL, 
+  password VARCHAR(60) NOT NULL
+)
