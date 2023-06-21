0b0b6bcb48cee35faeb154455e81e6c3ff9b25d9
Bunyodbek <93827769+BunyodbekNasriddinov@users.noreply.github.com>
2023-06-21 17:56:26 +0500

    Update branch.model.js

--- src/models/branch.model.js
+++ src/models/branch.model.js
@@ -3,6 +3,7 @@ import { fetchAll } from "../utils/pg.js"
 const newBranchQuery = `
   INSERT INTO branch(branch_name, branch_phone, branch_address) 
   VALUES($1, $2, $3)
+  RETURNING *
   `
 
 async function newBranch({ branch_name, branch_phone, branch_address }) {
