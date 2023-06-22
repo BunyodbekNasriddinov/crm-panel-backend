6e86934871a4d54a29d9d85d5f200e0cd8bef047
muhammadqodirmuminov <muminovmuhammadqodir2@gmail.com>
2023-06-22 10:13:20 +0500

    course add && get && validation

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,5 +1,11 @@
+const PORT = process.env.PORT || 5001;
+
+export { PORT };
+
+
 export default {
   pg: {
     connectionString: process.env.PG_CONNECTION_STRING,
   },
 };
+
--- src/models/admin.model.js
+++ src/models/admin.model.js
@@ -1,8 +1,23 @@
-import {fetchAll} from '../utils/pg.js';
+import { fetchAll } from '../utils/pg.js';
 
 const getAdmin = async () => {
-    const data = await fetchAll('SELECT * FROM admins')
-        return data
-}
+	const data = await fetchAll('SELECT * FROM admins');
+	return data;
+};
 
-export default getAdmin
+const getCourses = async () => {
+	const data = await fetchAll(
+		'SELECT * FROM courses AS c INNER JOIN branch AS b ON c.branch_id = b.branch_id;'
+	);
+	return data;
+};
+
+const postCourses = async (name, price, branch_id, image) => {
+	const data = await fetchAll(
+		'INSERT INTO courses(course_name, course_price, branch_id, course_image) VALUES ($1,$2, $3, $4) RETURNING *',
+		[name, price, branch_id, image]
+	);
+	return data;
+};
+
+export default { getAdmin, getCourses, postCourses };
